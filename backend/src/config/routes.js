ed82f7b1a36de2b444cc43080849ba1fcecd7a5a
Thiago Yamaguchi <thiago.yamaguchi@gmail.com>
2019-02-27 14:06:17 -0300

    pasta backend

---
+++ backend/src/config/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+module.exports = function(server){
+    //API Routes
+    const router = express.Router()
+    server.use('/api', router)
+
+    //TODO Routes
+    const todoServide = require('../api/todo/todoService')
+    todoServide.register(router, '/todos')
+}
